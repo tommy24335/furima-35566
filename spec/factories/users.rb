a2857ea91e99732b7e1f29653fb54451b883d759
tommy24335 <a12799866@gmail.com>
2021-05-19 17:05:25 +0900

    with_optionsをネスト、単体テストコードの追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,13 @@
 FactoryBot.define do
   factory :user do
-
-    nickname              {'tommy'}
-    email                 {'test@example'}
-    password              {'000aaa'}
-    password_confirmation {password}
-    famiry_name           {'冨永'}
-    last_name             {'遼太'}
-    famiry_name_kana      {'トミナガ'}
-    last_name_kana        {'リョウタ'}
-    birth_day             {'1996-02-05'}
+    nickname              { 'tommy' }
+    email                 { 'test@example' }
+    password              { '000aaa' }
+    password_confirmation { password }
+    famiry_name           { '冨永' }
+    last_name             { '遼太' }
+    famiry_name_kana      { 'トミナガ' }
+    last_name_kana        { 'リョウタ' }
+    birth_day             { '1996-02-05' }
   end
 end
