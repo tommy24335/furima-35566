f4a8c84e3cef41a686e70a6ce2c36de061566ecf
tommy24335 <a12799866@gmail.com>
2021-05-25 19:00:36 +0900

    fixed items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,    only: [:new, :create, :edit]
+  before_action :authenticate_user!,    only: [:new, :create, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
   before_action :not_access, only: [:edit, :update]
 
