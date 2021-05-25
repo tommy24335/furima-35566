b4f5debe4808f5a08b7ee5ad0eb88b7e82b36e50
tommy24335 <a12799866@gmail.com>
2021-05-25 21:36:49 +0900

    fixed destroyアクション#2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,    except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :not_access, only: [:edit, :update]
+  before_action :not_access, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
