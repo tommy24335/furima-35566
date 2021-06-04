c00f336a5592c4b2659db25df9720eeadcbecb27
tommy24335 <a12799866@gmail.com>
2021-06-04 14:49:01 +0900

    fixed item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,8 +21,7 @@ class Item < ApplicationRecord
       validates :category_id
       validates :item_status_id
     end
-    validates :price, numericality: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' }
-    validates :price,
+    validates :price, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' },
               numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
                               message: ' is out of setting range' }
   end
